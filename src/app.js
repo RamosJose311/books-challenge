817ea62149ba7081badfe7fdadc8ce04e5ae413b
Ramosjose311 <joseezequiel01@hotmail.es>
2023-01-16 18:32:54 -0300

    terminacion del trabajo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mainRouter = require('./routes/main');
+const methodOverride =require('method-override');
+const session = require('express-session');
 
 const app = express();
 
@@ -9,6 +11,17 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 
+app.use(methodOverride('_method'));
+app.use(session({
+  secret : 'challenge-Book',
+  resave: true,
+  saveUninitialized : true,
+  saveUninitiañizedState : true,
+}));
+app.use((req,res,next) =>{
+  req.session.userLogin ? res.locals.userLogin = req.session.userLogin : null
+  next()
+})
 app.use('/', mainRouter);
 
 app.listen(3000, () => {
--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,5 +1,6 @@
 const bcryptjs = require('bcryptjs');
 const db = require('../database/models');
+const {Op} =require("sequelize");
 
 const mainController = {
   home: (req, res) => {
@@ -13,18 +14,47 @@ const mainController = {
   },
   bookDetail: (req, res) => {
     // Implement look for details in the database
-    res.render('bookDetail');
+    const {id} = req.params ;
+    db.Book.findByPk(id,{include :["authors"]})
+    .then((book)  =>{
+      res.render("bookDetail",{
+        book,
+      });
+    })
+    .catch((error) => console.log(error));
   },
   bookSearch: (req, res) => {
     res.render('search', { books: [] });
   },
   bookSearchResult: (req, res) => {
     // Implement search by title
-    res.render('search');
+    db.Book.findAll({
+      where: {
+        title: {
+          [Op.substring]: req.body.title,
+        },
+      },
+      include: ["authors"],
+    })
+    .then((books)=>{
+      return res.render("search",{
+        books,
+        keywords:req.body.title,
+      });
+    })
+    .catch((error)=> console.log(error));
   },
   deleteBook: (req, res) => {
     // Implement delete book
-    res.render('home');
+    db.book.destroy({
+      where:{
+        id:req.params.id,
+      },
+    })
+    .then(()=>{
+      return res.redirect("/");
+    })
+    .catch((error)=> console.log(error))
   },
   authors: (req, res) => {
     db.Author.findAll()
@@ -35,7 +65,14 @@ const mainController = {
   },
   authorBooks: (req, res) => {
     // Implement books by author
-    res.render('authorBooks');
+    const {id} =req.params;
+    db.Author.findByPk(id,{
+      include:["books"],
+    })
+    .then((author)=>{
+      return res.render("authorbooks",{author});
+    })
+    .catch((error)=> console.log(error));
   },
   register: (req, res) => {
     res.render('register');
@@ -59,15 +96,58 @@ const mainController = {
   },
   processLogin: (req, res) => {
     // Implement login process
-    res.render('home');
+    db.User.findOne({
+      where: {
+        email:req.body.email.trim(),
+      },
+    })
+    .then((user)=>{
+      if(!user || !bcryptjs.compareSync(req.body.password,user.Pass)){
+        return res.render("home",{
+          error : "credenciales invalidas",
+        });
+      } else{
+        console.log(user.email)
+        req.session.userLogin ={
+          name:user.name,
+          rol:user.categoryId,
+        };
+        res.locals.userLogin = req.session.userLogin;
+        return res.redirect("/");
+      }
+    })
+    .catch((error) => console.log(error));
+  },
+  logout:(req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
   },
   edit: (req, res) => {
     // Implement edit book
-    res.render('editBook', {id: req.params.id})
+    db.Book.findByPk(req.params.id)
+    .then((book)=>{
+      return res.render("editBook",{book});
+    })
   },
   processEdit: (req, res) => {
     // Implement edit book
-    res.render('home');
+    const {title,cover,description} =req.body;
+    db.Book.update(
+      {
+        title : title.trim(),
+        cover: cover.trim(),
+        description:description.trim(),
+      },
+      {
+        where: {
+          id:req.params.id,
+        },
+      }
+    )
+    .then(()=>{
+      res.redirect("/");
+    })
+    .catch((error) => console.log(error));
   }
 };
 
